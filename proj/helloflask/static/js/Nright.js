841343ba7f11c4602d6b3922e3be696e75b27cda
woowoo <102814823+dnwnssla@users.noreply.github.com>
2023-06-09 16:49:49 +0900

    first

---
+++ proj/helloflask/static/js/Nright.js
@@ -0,0 +1,12 @@
+
+function openMenu() {
+    document.getElementById("main").style.marginLeft = "250px";
+    document.querySelector('.sidebar').style.width = "250px";
+    document.querySelector('.openbtn').style.display = 'none';
+    }
+    
+function closeMenu() { 
+    document.getElementById("main").style.marginLeft= "0";
+    document.querySelector('.sidebar').style.width = "0";
+    document.querySelector('.openbtn').style.display = 'block';
+                }
