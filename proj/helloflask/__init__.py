629d7687e08a4c4e245ae6fccd5bdc3fea6127bb
woowoo <102814823+dnwnssla@users.noreply.github.com>
2023-06-14 17:35:13 +0900

    6/14

--- proj/helloflask/__init__.py
+++ proj/helloflask/__init__.py
@@ -21,7 +21,7 @@ def login_post():
     log = Diagnosi.query.filter(Diagnosi.diag_id==diag_f).first()
     if log is not None:
         pait = Paitent.query.filter(Paitent.pat_id == log.pat_id).first()
-        session['loginUser'] = { 'userid':log.diag_id, 'name': pait.pat_name }
+        session['loginUser'] = { 'userid':log.diag_id, 'name': pait.pat_name ,'pat_id': pait.pat_id}
         if session.get('next'): 
             next = session.get('next')
             del session['next']
@@ -43,21 +43,53 @@ def logout():
 def home():    
     return render_template('home.html',title="main")
 
-@app.route('/hospital')
+@app.route('/hospital',methods=['GET'])
 def hospital():
     item = EvaluteItem.query.filter(EvaluteItem.target == "병원").order_by(EvaluteItem.type).all()
     mlist=["매우만족", "만족", "보통", "불만족", "매우불만족"]
-#    print(item)   
+
     return render_template('hoste.html',evallist=item, mlist=mlist)
 
-@app.route('/doctor')
+@app.route('/hospital',methods=['POST'])
+def hospital_post():
+    eval_list = EvaluteItem.query.filter(EvaluteItem.target == "병원").order_by(EvaluteItem.type).all()
+    userinfo= session.get('loginUser')
+    pid=userinfo['pat_id']
+    did =userinfo['userid']
+   
+    for item in eval_list:  
+        item_value = request.form.get(str(item.evalu_id))      
+        u = HosEvalu(pat_id = pid, diag_id = did,evalu_id =item.evalu_id, evalu_ans=item_value)
+        db_session.add(u)
+    db_session.commit()
+    return render_template('home.html')
+    
+@app.route('/doctor', methods=['GET'])
 def doctor():
-    diag = DiagDoc.query.filter(DiagDoc.diag_id,DiagDoc.pat_id,DiagDoc.doc_id).all()
-    doc = Doctor.query.filter(Doctor.doc_id,Doctor.doc_name,Doctor.doc_major).all()
+    diag = DiagDoc.query.filter(DiagDoc.diag_id,DiagDoc.pat_id,DiagDoc.doc_id)
+    doc = Doctor.query.filter(Doctor.doc_id==DiagDoc.doc_id).all()
     item = EvaluteItem.query.filter(EvaluteItem.target == "의사").order_by(EvaluteItem.type).all()
     mlist=["매우만족", "만족", "보통", "불만족", "매우불만족"]
 #    print(item)   
-    return render_template('doce.html',evallist=item, mlist=mlist)    
+    return render_template('doce.html',evallist=item, mlist=mlist, doclist= doc)    
+
+@app.route('/doctor', methods=['POST'])
+def doctor_post():
+    doc = Doctor.query.filter(Doctor.doc_id==DiagDoc.doc_id).all()
+    evalu_list = EvaluteItem.query.filter(EvaluteItem.target == "의사").order_by(EvaluteItem.type).all()
+    userinfo = session.get('loginUser')
+    pid=userinfo['pat_id']  
+    did = userinfo['userid']
+    for doc_list in doc:
+        for item in evalu_list:
+            item_value=request.form.get(str(item.evalu_id))
+            du = DocEvalu(pat_id = pid, diag_id = did, doc_id = doc_list.doc_id, evalu_id = item.evalu_id, evalu_ans=item_value)
+            db_session.add(du)
+        db_session.commit()
+    
+    return render_template('home.html')   
+
+
 
 @app.route('/reservation')
 def reservation():
